c2694935e478064040a4363d5c1daa438c993b54
Ruben Müller <mueller@91interactive.com>
2023-07-30 20:05:13 +0200

    use query only for get calls

--- src/Api/Utils/ApiClient.php
+++ src/Api/Utils/ApiClient.php
@@ -15,86 +15,90 @@ use GuzzleHttp\Client;
 
 class ApiClient
 {
-    private $client;
+	private $client;
 
-    private function getClient(): Client
-    {
-        if (!$this->client) {
-            $this->client = new Client([
-                'base_uri' => $this->baseUrl(),
-            ]);
-        }
-        return $this->client;
-    }
+	private function getClient(): Client
+	{
+		if (!$this->client) {
+			$this->client = new Client([
+				'base_uri' => $this->baseUrl(),
+			]);
+		}
+		return $this->client;
+	}
 
-    private function getToken(): string
-    {
-        return config('sevdesk-api.api_token');
-    }
+	private function getToken(): string
+	{
+		return config('sevdesk-api.api_token');
+	}
 
-    private function baseUrl()
-    {
-        return 'https://my.sevdesk.de';
-    }
+	private function baseUrl()
+	{
+		return 'https://my.sevdesk.de';
+	}
 
-    public function execute($httpMethod, $url, array $parameters = [])
-    {
-        try {
-            $parameters['token'] = $this->getToken();
-            $response = $this->getClient()->{$httpMethod}('api/v1/' . $url, ['query' => $parameters]);
-            $responseBody = json_decode((string)$response->getBody(), true);
-            return $responseBody;
-            // return $responseBody['objects'];
-        } catch (BadResponseException $exception) {
-            $response = json_decode((string)$exception->getResponse()->getBody(), true);
-            if (array_key_exists('error', $response)) {
-                if ($response['error']['code'] == 151) throw new ModelNotFoundException($response['error']['message']);
-                if ($response['error']['message'] != null) throw new \Exception($response['error']['message']);;
-            }
-            if (array_key_exists('status', $response)) {
-                if ($response['status'] == 401) throw new UnauthorizedException($response['message']);
-            }
-            throw new \Exception('Something went wrong.');
-        }
+	public function execute($httpMethod, $url, array $parameters = [])
+	{
+		try {
+			$parameters['token'] = $this->getToken();
+			if ($httpMethod === 'get')
+				$response = $this->getClient()->{$httpMethod}('api/v1/' . $url, ['query' => $parameters]);
+			else
+				$response = $this->getClient()->{$httpMethod}('api/v1/' . $url, ['form_params' => $parameters]);
+			$responseBody = json_decode((string)$response->getBody(), true);
+			return $responseBody;
+			// return $responseBody['objects'];
+		} catch (BadResponseException $exception) {
+			$response = json_decode((string)$exception->getResponse()->getBody(), true);
+			if ($response) {
+				if (array_key_exists('error', $response)) {
+					if ($response['error']['code'] == 151) throw new ModelNotFoundException($response['error']['message']);
+					if ($response['error']['message'] != null) throw new \Exception($response['error']['message']);;
+				}
+				if (array_key_exists('status', $response)) {
+					if ($response['status'] == 401) throw new UnauthorizedException($response['message']);
+				}
+			}
+			throw new \Exception('Something went wrong.');
+		}
+	}
 
-    }
+	// ========================= base methods ======================================
 
-    // ========================= base methods ======================================
+	public function _get($url = null, $parameters = [])
+	{
+		return $this->execute('get', $url, $parameters);
+	}
 
-    public function _get($url = null, $parameters = [])
-    {
-        return $this->execute('get', $url, $parameters);
-    }
+	public function _post($url = null, array $parameters = [])
+	{
+		return $this->execute('post', $url, $parameters);
+	}
 
-    public function _post($url = null, array $parameters = [])
-    {
-        return $this->execute('post', $url, $parameters);
-    }
+	public function _put($url = null, array $parameters = [])
+	{
+		return $this->execute('put', $url, $parameters);
+	}
 
-    public function _put($url = null, array $parameters = [])
-    {
-        return $this->execute('put', $url, $parameters);
-    }
+	public function _patch($url = null, array $parameters = [])
+	{
+		return $this->execute('patch', $url, $parameters);
+	}
 
-    public function _patch($url = null, array $parameters = [])
-    {
-        return $this->execute('patch', $url, $parameters);
-    }
+	public function _delete($url = null, array $parameters = [])
+	{
+		return $this->execute('delete', $url, $parameters);
+	}
 
-    public function _delete($url = null, array $parameters = [])
-    {
-        return $this->execute('delete', $url, $parameters);
-    }
+	// =========================== helper ===========================================
 
-    // =========================== helper ===========================================
+	const INVOICE = 'Invoice';
+	const CREDIT_NOTE = 'CreditNote';
+	const ORDER = 'Order';
 
-    const INVOICE = 'Invoice';
-    const CREDIT_NOTE = 'CreditNote';
-    const ORDER = 'Order';
-
-    public function getNextSequence(string $objectType = self::INVOICE)
-    {
-        $sequence = $this->_get(Routes::SEQUENCE, ['objectType' => $objectType])['objects'];
-        return str_replace('%NUMBER', $sequence['nextSequence'], $sequence['format']);
-    }
+	public function getNextSequence(string $objectType = self::INVOICE)
+	{
+		$sequence = $this->_get(Routes::SEQUENCE, ['objectType' => $objectType])['objects'];
+		return str_replace('%NUMBER', $sequence['nextSequence'], $sequence['format']);
+	}
 }
